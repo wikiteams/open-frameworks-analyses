a5f30bd3c3cae608ebd8a1733a0e5e0c89f4f2eb
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-27 23:46:54 +0100

    Working on retriever.py

--- retriever.py
+++ retriever.py
@@ -27,26 +27,78 @@ SOFTWARE.
 import sys
 import urllib
 import csv
-# import PyGithub
+from github import Github, UnknownObjectException, GithubException
 # import ElementTree based on the python version
 try:
     import elementtree.ElementTree as ET
 except ImportError:
     import xml.etree.ElementTree as ET
 
+
+pagination = 10
+
+
+def num_modulo(thread_id_count__):
+    global no_of_threads
+    return thread_id_count__ % pagination
+
+
 # don't forget to provide api key as first arg of python script
 results_done = 0
 results_all = 8420  # checked manually, hence its later overwritten
-pagination = 10
 page = 0
+
+timeout = 50
+
+github_clients = list()
+github_clients_ids = list()
+
+secrets = []
+
+credential_list = []
+# reading the secrets, the Github factory objects will be created in next paragraph
+with open('pass.txt', 'r') as passfile:
+    line__id = 0
+    for line in passfile:
+        line__id += 1
+        secrets.append(line)
+        if line__id % 4 == 0:
+            login_or_token__ = str(secrets[0]).strip()
+            pass_string = str(secrets[1]).strip()
+            client_id__ = str(secrets[2]).strip()
+            client_secret__ = str(secrets[3]).strip()
+            credential_list.append({'login': login_or_token__, 'pass': pass_string, 'client_id': client_id__, 'client_secret': client_secret__})
+            del secrets[:]
+
+print str(len(credential_list)) + ' full credentials successfully loaded'
+
+# with the credential_list list we create a list of Github objects, github_clients holds ready Github objects
+for credential in credential_list:
+    local_gh = Github(login_or_token=credential['pass'], client_id=credential['client_id'],
+                      client_secret=credential['client_secret'], user_agent=credential['login'],
+                      timeout=timeout)
+    github_clients.append(local_gh)
+    github_clients_ids.append(credential['login'])
+    print local_gh.rate_limiting
+
+print 'How many Github objects in github_clients: ' + str(len(github_clients))
+print 'Assigning current github client to the first object in a list'
+
+github_client = github_clients[0]
+lapis = local_gh.get_api_status()
+print 'Current status of GitHub API...: ' + lapis.status + ' (last update: ' + str(lapis.last_updated) + ')'
 
 with open('results.csv', 'wb') as csv_file:
     csv_writer = csv.writer(csv_file, delimiter=';', quotechar='\"', quoting=csv.QUOTE_ALL)
-    headers = ['ordinal_id', 'project_id', 'project_name', 'project_url', 'project_htmlurl', 'project_created_at',
+    headers = ['ordinal_id', 'github_id', 'forks_count', 'stargazers_count', 'project_id',
+               'project_name', 'project_url', 'project_htmlurl', 'project_created_at',
                'project_updated_at', 'project_homepage_url', 'project_average_rating', 'project_rating_count', 'project_review_count',
                'project_activity_level', 'project_user_count', 'twelve_month_contributor_count', 'total_contributor_count',
-               'twelve_month_commit_count', 'total_commit_count', 'total_code_lines']
+               'twelve_month_commit_count', 'total_commit_count', 'total_code_lines', 'main_language_name']
     csv_writer.writerow(headers)
+
+    Github(login_or_token=credential['pass'], client_id=credential['client_id'],
+           client_secret=credential['client_secret'], user_agent=credential['login'], timeout=timeout)
 
     while (results_done < results_all):
         # Connect to the Ohloh website and retrieve the account data.
@@ -94,6 +146,46 @@ with open('results.csv', 'wb') as csv_file:
 
             project_user_count = node.find("user_count").text
 
+            # project may have multiple GitHub repositories
+            # or even it may be not present on GitHub - check that
+
+            is_github_project = False
+            github_repo_id = None
+
+            # in case of multiple github CODE repositories (quite often)
+            # treat as a seperate repo - remember, we focus on github repositories, not aggregates
+
+            enlistments_detailed_params = urllib.urlencode({'api_key': sys.argv[1]})
+            enlistments_detailed_url = "https://www.openhub.net/projects/%s/enlistments.xml?%s" % (project_id, enlistments_detailed_params)
+
+            enlistments_result_flow = urllib.urlopen(enlistments_detailed_url)
+
+            # Parse the response into a structured XML object
+            enlistments_tree = ET.parse(enlistments_result_flow)
+
+            # Did Ohloh return an error?
+            enlistments_elem = enlistments_tree.getroot()
+            enlistments_error = enlistments_elem.find("error")
+            if enlistments_error is not None:
+                print 'Ohloh returned:', ET.tostring(enlistments_error),
+                sys.exit()
+
+            repos_lists = list()
+
+            for enlistment_node in enlistments_elem.findall("result/enlistment"):
+                ee_type = enlistment_node.find("repository/type").text
+                if (ee_type == "GitRepository"):
+                    ee_link = enlistment_node.find("repository/url").text
+                    if (ee_link.startswith("git://github.com/")):
+                        print 'Is a GitHub project!'
+                        is_github_project = True
+                        github_repo_id = ee_link.split("git://github.com/")[1].split(".git")[0]
+                        print github_repo_id
+                        repos_lists.append(github_repo_id)
+
+            if not is_github_project:
+                continue
+
             # now lets get even more sophisticated details
             params_detailed_url = urllib.urlencode({'api_key': sys.argv[1]})
             project_detailed_url = "https://www.openhub.net/projects/%s.xml?%s" % (project_id, params_sort_rating)
@@ -115,14 +207,32 @@ with open('results.csv', 'wb') as csv_file:
             twelve_month_commit_count = detailed_elem.find("result/project/analysis/twelve_month_commit_count").text
             total_commit_count = detailed_elem.find("result/project/analysis/total_commit_count").text
             total_code_lines = detailed_elem.find("result/project/analysis/total_code_lines").text
+            main_language_name = detailed_elem.find("result/project/analysis/main_language_name").text
 
-            collection = [str(((page-1)*pagination) + i), project_id, project_name, project_url, project_htmlurl, project_created_at,
-                          project_updated_at, project_homepage_url, project_average_rating, project_rating_count, project_review_count,
-                          project_activity_level, project_user_count, twelve_month_contributor_count, total_contributor_count,
-                          twelve_month_commit_count, total_commit_count, total_code_lines]
+            current_ghc = github_clients[num_modulo(i-1)]
+            current_ghc_desc = github_clients_ids[num_modulo(i-1)]
 
-            csv_writer.writerow(collection)
-            print '.'
+            for gh_entity in repos_lists:
+
+                try:
+                    repository = current_ghc.get_repo(gh_entity)
+                    stargazers_count = repository.stargazers_count
+                    forks_count = repository.forks_count
+
+                    collection = [str(((page-1)*pagination) + i), gh_entity, str(forks_count),
+                                  str(stargazers_count), project_id, project_name, project_url,
+                                  project_htmlurl, project_created_at,
+                                  project_updated_at, project_homepage_url, project_average_rating, project_rating_count, project_review_count,
+                                  project_activity_level, project_user_count, twelve_month_contributor_count, total_contributor_count,
+                                  twelve_month_commit_count, total_commit_count, total_code_lines, main_language_name]
+
+                    csv_writer.writerow(collection)
+                    print '.'
+                except UnknownObjectException:
+                    print 'Repo ' + gh_entity + ' is not available anymore..'
+                except GithubException:
+                    # TODO: write here something clever
+                    raise
 
         # Output all the immediate child properties of an Account
         # for node in elem.find("result/project"):

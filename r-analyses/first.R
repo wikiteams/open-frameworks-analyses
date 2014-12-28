f38aacc6bec297ad75dacd57662764c530bd7cbf
Oskar Jarczyk <s4268@pjwstk.edu.pl>
2014-12-28 13:57:25 +0100

    Updated required R packages. Changes in first.R

--- r-analyses/first.R
+++ r-analyses/first.R
@@ -1,4 +1,12 @@
 install.packages("knitr")
+# Elegant, flexible and fast dynamic report generation with R
+# make sure to set knitr in Tools -> Global Options -> Sweave
 install.packages("ggplot2")
+# ggplot2 is a plotting system for R, based on the grammar of graphics
 install.packages("xtable")
+# xtable: Export tables to LaTeX or HTML
+# coerce data to LaTeX and HTML tables
 install.packages("ROCR")
+# visualizing classifier performance in R, with only 3 commands
+install.packages("lme4")
+# lme4: Linear mixed-effects models using Eigen and S4
